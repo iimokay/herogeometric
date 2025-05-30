9ac31d1d713d7958efe528ea67c8ae8ffbcd4b18
ImOkay <imalec.huang@gmail.com>
2025-05-30 17:24:14 +0800

    fix prompt

--- app/api/ad/analysis/route.ts
+++ app/api/ad/analysis/route.ts
@@ -1,3 +1,4 @@
+import { parseJSONObjectFromText } from '@/lib/parsing';
 import { openai } from '@ai-sdk/openai';
 import { CoreMessage, generateText } from 'ai';
 import { NextResponse } from 'next/server';
@@ -5,33 +6,43 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
     try {
         const body = await req.json();
-        const { content, media, } = body;
-        // Use regex to match MIME type
-        const regex = /data:([^;]+);/;
-        const match = media.match(regex);
-        const mimeType = match && match[1] ? match[1] : "image/jpeg";
-        console.log("⬇️ analysis content:", content, media.substring(0, 50), mimeType);
+        const { content, media } = body;
+        console.log("⬇️ analysis content:", content);
         // generate image description
         const messages: CoreMessage[] = [
             {
                 role: "system",
-                content: "I'm a helpful assistant that can help you analyze the image."
+                content: `You are a content creator and create a piece of content based on the following\n`
+                    + `\nResponse format should be formatted in a valid JSON block like this:
+\`\`\`json
+{ "theme": "<string>", "keywords": "<string[3-5]>", "text": "<string>" }
+\`\`\`
+
+The "theme" field should be categorization of content themes,short and concise.
+The "keywords" field should be an array of keywords that are relevant to the content.
+The "text" field should be the content of the piece of content,short and concise.
+
+The content should be in the language of the user.`
             },
             {
                 role: "user",
-                content: [{ type: "image", image: media, mimeType }],
-            },
+                content: [
+                    { type: "text", text: content },
+                    { type: "image", image: media[0].data, mimeType: media[0].type }
+                ],
+            }
         ];
 
-        const descriptionResult = await generateText({
+        const result = await generateText({
             model: openai('gpt-4o-mini'),
             messages
         });
 
+        const analysisResult = parseJSONObjectFromText(result.text);
+
         return NextResponse.json({
-            imageDescription: descriptionResult.text,
-            content: content,
-            reasoning: descriptionResult.reasoning
+            analysisResult,
+            reasoning: result.reasoning
         });
     } catch (error) {
         console.error('Error generating content:', error);
